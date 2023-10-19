0a5e3c09a05117e7e2c2ddbe9d5c112f1f8bb923
ahteshamniqosa <147423967+ahteshamniqosa@users.noreply.github.com>
2023-10-19 22:42:15 +0500

    //

---
+++ resources/js/views/admin/CreatorCampaign.vue
@@ -0,0 +1,268 @@
+<template>
+  <div class="contanir">
+    <div class="row" style="padding-top: 30px;">
+      <div class="col-4">
+            <div class="card">
+              <div class="card-body-2">
+                <div class="img">
+                  <img src="/images/user (1).png" style="width: 75%; padding-left: 12px;"/>
+                </div>
+               <h5 class="card-title">173</h5>
+                 <p class="card-text">Admin to Canditate Ticket</p>
+                </div>
+            </div>
+          </div>
+          <div class="col-4">
+            <div class="card">
+              <div class="card-body-2">
+                <div class="img">
+                  <img src="/images/user (1).png" style="width: 75%; padding-left: 12px;"/>
+                </div>
+               <h5 class="card-title">173</h5>
+                 <p class="card-text">Admin to Canditate Ticket</p>
+                </div>
+            </div>
+          </div>
+           <div class="col-4">
+            <div class="card">
+              <div class="card-body-2">
+                <div class="img">
+                  <img src="/images/user (1).png" style="width: 75%;padding-left: 12px;"/>
+                </div>
+               <h5 class="card-title">173</h5>
+                 <p class="card-text">Admin to Canditate Ticket</p>
+                </div>
+            </div>
+          </div>
+    </div>
+    <div class="row" style="padding-top: 20px;">
+      <div class="col-4">
+            <div class="card">
+              <div class="card-body-2">
+                <div class="img">
+                  <img src="/images/user (1).png" style="width: 75%;padding-left: 12px;"/>
+                </div>
+               <h5 class="card-title">173</h5>
+                 <p class="card-text">Admin to Canditate Ticket</p>
+                </div>
+            </div>
+          </div>
+          <div class="col-4">
+            <div class="card">
+              <div class="card-body-2">
+                <div class="img">
+                  <img src="/images/user (1).png" style="width: 75%;padding-left: 12px;"/>
+                </div>
+               <h5 class="card-title">173</h5>
+                 <p class="card-text">Admin to Canditate Ticket</p>
+                </div>
+            </div>
+          </div>
+          <div class="col-4">
+            <div class="card">
+              <div class="card-body-2">
+                <div class="img">
+                  <img src="/images/user (1).png" style="width: 75%;padding-left: 12px;"/>
+                </div>
+               <h5 class="card-title">173</h5>
+                 <p class="card-text">Admin to Canditate Ticket</p>
+                </div>
+            </div>
+          </div>
+    </div>
+    <div class="row" style="padding-top: 30px;">
+      <div class="col-6 Country">
+        <div class="col ul-title">
+          <h4>Location</h4>
+          <p>2023-10-13</p>
+        </div>
+        <ul>
+          <li class="li_list">
+            <div class="li_img"><img src="/images/spain-c.png" style="width: 30%;"/><p>Spain</p></div>
+            <p>142</p>
+          </li>
+          <li class="li_list">
+            <div class="li_img"><img src="/images/morocco-c.png" style="width: 26%;"/><p>Morocco</p></div>
+            <p>77</p>
+          </li>
+          <li class="li_list">
+            <div class="li_img"><img src="/images/ivory-coast-c.png" style="width: 23%;"/><p>Ivory-coast</p></div>
+            <p>131</p>
+          </li>
+          <li class="li_list">
+            <div class="li_img"><img src="/images/tunisia-c.png" style="width: 30%;"/><p>Tunisia</p></div>
+            <p>30</p>
+          </li>
+        </ul>
+      </div>
+      <div class="col-6 Country">
+        <div class="col ul-title">
+          <h4>Company Types</h4>
+          <p>2023-10-13</p>
+        </div>
+        <ul>
+          <li class="li_list">
+            <div class="li_img"><img src="/images/instagram-c.png" style="width: 25%;"/><p>Instagram</p></div>
+            <p>142</p>
+          </li>
+          <li class="li_list">
+            <div class="li_img"><img src="/images/chrome-c.png" style="width: 28%;"/><p>Google</p></div>
+            <p>142</p>
+          </li>
+          <li class="li_list">
+            <div class="li_img"><img src="/images/ivory-coast-c2.png"  style="width: 23%;"/><p>Country Name</p></div>
+            <p>142</p>
+          </li>
+          <li class="li_list">
+            <div class="li_img"><img src="/images/messsag-c.png"  style="width: 23%;"/><p>Country Name</p></div>
+            <p>142</p>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="row" style="padding-top: 30px;">
+      <div class="col-6 Country">
+        <div class="col ul-title">
+          <h4>Sector of your company</h4>
+          <p>2023-10-13</p>
+        </div>
+        <ul>
+          <li class="li_list">
+            <div class="li_img"><img src="/images/spin-1.png" style="width: 28%;"/><p>Clothes</p></div>
+            <p>142</p>
+          </li>
+          <li class="li_list">
+            <div class="li_img"><img src="/images/Group 536.png" style="width: 18%;"/><p>Food and Drinks</p></div>
+            <p>77</p>
+          </li>
+          <li class="li_list">
+            <div class="li_img"><img src="/images/Group 537.png" style="width: 22%;"/><p>Home Items</p></div>
+            <p>131</p>
+          </li>
+          <li class="li_list">
+            <div class="li_img"><img src="/images/Group 538.png" style="width: 28%;"/><p>Sports</p></div>
+            <p>30</p>
+          </li>
+        </ul>
+      </div>
+      <div class="col-6 Country">
+        <div class="col ul-title">
+          <h4>How did you know Yallad</h4>
+          <p>2023-10-13</p>
+        </div>
+        <ul>
+          <li class="li_list">
+            <div class="li_img"><img src="/images/c1.png" style="width: 20%;"/><p>E-commerce</p></div>
+            <p>142</p>
+          </li>
+          <li class="li_list">
+            <div class="li_img"><img src="/images/c2.png" style="width: 28%;"/><p>Agency</p></div>
+            <p>142</p>
+          </li>
+          <li class="li_list">
+            <div class="li_img"><img src="/images/Group 541.png"  style="width: 23%;"/><p>Mobile app</p></div>
+            <p>142</p>
+          </li>
+          <li class="li_list">
+            <div class="li_img"><img src="/images/Group 540.png"  style="width: 23%;"/><p>Restaurateur</p></div>
+            <p>142</p>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import chartAreaDemo from "../../chart/demo/chart-area-demo";
+import chartPieDemo from "../../chart/demo/chart-pie-demo";
+import Vue from 'vue'
+
+  import { get , byMethod} from '../admin/components/lib/api'
+  import Typehead from '../admin/components/typehead/Typehead.vue'
+
+export default {
+  components: { Typehead },
+data () {
+          return {
+              form: {},
+              users_data:{},
+              model: {
+                  data: []
+              }
+          }
+      },
+name: "Dashboard",
+methods: {}
+};
+</script>
+
+<style scoped>
+.card{
+     width: 100%; 
+      border: none;
+     border-radius: 10px;
+     box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+}
+.card-text{
+     font-size: 15px;color: #000;
+     font-weight: 200;
+     text-align: center;
+}
+.card-body-2{
+  display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 10px 0px;
+}
+.card-title{
+font-size: 18px;
+color: #000;
+font-weight: 200; 
+padding-top: 8px;
+}
+.li_img{
+    display: flex;
+    gap: 0px 15px;
+    align-items: center;
+
+}
+.li_img  p{
+  font-size: 18px;
+  color: #000;
+  font-weight: 400;
+  padding-top: 10px;
+}
+.Country ul{
+  list-style: none;
+  padding-top: 24px;
+}
+.li_list{
+     display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding-right: 24px;
+}
+.li_list p{
+  font-size: 18px;
+  font-weight: 400;
+  color: #000;
+  padding-top: 10px;
+}
+.ul-title{
+     display: flex;
+    justify-content: space-between;
+    align-items: center;
+    border-bottom: 1px solid #000;
+}
+.ul-title h4{
+  color: #000;
+    font-weight: 400;
+    padding-left: 30px;
+}
+.ul-title p {
+    padding-top: 10px;
+    font-size: 15px;
+    color: #000;
+    font-weight: 200;
+}
+</style>
